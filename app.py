a57902bd793d6c7a68f31bbe1ca654e9110a72c8
Diego <diego88aht@gmail.com>
2026-09-01 18:35:23 -0400

    Codigo basico para probar el puerto 8000 dentor y fuera del EC2

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html", name="Diego Andres")
+    return "Hello World!"
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-    
+    app.run(port=8000)
