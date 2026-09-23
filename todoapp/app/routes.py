e647778e55f770e1f9e82b3e635f0e070e95e810
Diego <diego88aht@gmail.com>
2026-09-23 16:19:28 -0400

    Agregar tareas funcionando en docker

--- todoapp/app/routes.py
+++ todoapp/app/routes.py
@@ -1,7 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
+
+from .extensions import db
+from .models import Todo
 
 bp = Blueprint("todos", __name__)
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        title = request.form.get("title", "").strip()
+
+        if title:
+            todo = Todo(title=title)
+            db.session.add(todo)
+            db.session.commit()
+
+        return redirect(url_for("todos.index"))
+    todos = Todo.query.order_by(Todo.created_at.desc()).all()
+    return render_template("index.html", todos=todos)
