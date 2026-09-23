f3169a7b1c059f5361e576d4a024393acd9254ae
Diego <diego88aht@gmail.com>
2026-09-23 12:10:13 -0400

    estrctura basica funcionando

---
+++ todoapp/app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+from .config import Config
+from .extensions import db, migrate
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from . import models
+    from .routes import bp as todos_bp
+
+    app.register_blueprint(todos_bp)
+
+    return app
